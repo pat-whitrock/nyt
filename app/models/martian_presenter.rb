87a676bbe978a8481f085d8a85153e3f4313e015
Patrick Whitrock <patrick.whitrock@betterment.com>
2018-03-11 01:39:40 -0500

    MartianPresenter cleanup

--- app/models/martian_presenter.rb
+++ app/models/martian_presenter.rb
@@ -1,11 +1,11 @@
 class MartianPresenter < SimpleDelegator
   MARTIANABLE_METHODS = %i(byline headline summary).freeze
-  MARTIANABLE_WORD = /[\w]{4,}/i.freeze
+  MARTIANABLE_WORD_PATTERN = /\w{4,}/i.freeze
 
   MARTIANABLE_METHODS.each do |method_name|
     define_method method_name do
-      super().gsub(MARTIANABLE_WORD) do |word|
-        if word.first.match(/[[:upper:]]/)
+      super().gsub(MARTIANABLE_WORD_PATTERN) do |string|
+        if string.first.match(/[[:upper:]]/)
           'Boinga'
         else
           'boinga'
